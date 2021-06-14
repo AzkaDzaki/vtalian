cd39a91ba901329b62716e2de1c70f37871febf3
AzkaDzaki <azkadzakiarrazak@gmail.com>
2021-06-14 10:30:59 +0700

    asd

--- js/indexs3.js
+++ js/indexs3.js
@@ -277,7 +277,7 @@ AFRAME.registerComponent('hotspots',{
     let wall_audio = await fetchMap("https://raw.githubusercontent.com/virtualtouralian/vtalian_file/main/json/wall-audio.json");
 wall_audio.wall_audio.forEach(element => {
   const audio = document.createElement('a-box');
-  setAttributes(audio, {"id": element.id, "rotation": element.rotation, "position": element.position, "sound": element.sound+"volume:5", "class": "audio-entity pause", "wall-audio":"", "geometry":"height: 1.000; width: 0.100", "material":"src: https://raw.githubusercontent.com/virtualtouralian/vtalian_file/main/img/topcoat_audio.png;shader:flat; color: #FFF; transparent: true;", "crossorigin":"anonymous", "animation__scale":"property: scale; to: 1.1 1.1 1.1; dur: 200; startEvents: mouseenter", "animation__scale_reverse":"property: scale; to: 1 1 1; dur: 200; startEvents: mouseleave"})
+  setAttributes(audio, {"id": element.id, "rotation": element.rotation, "position": element.position, "sound": element.sound+"volume:5", "class": "audio-entity pause", "geometry":"height: 1.000; width: 0.100", "material":"src: https://raw.githubusercontent.com/virtualtouralian/vtalian_file/main/img/topcoat_audio.png;shader:flat; color: #FFF; transparent: true;", "crossorigin":"anonymous", "animation__scale":"property: scale; to: 1.1 1.1 1.1; dur: 200; startEvents: mouseenter", "animation__scale_reverse":"property: scale; to: 1 1 1; dur: 200; startEvents: mouseleave"})
   document.getElementById("wall_audio").appendChild(audio);
 });
   }
